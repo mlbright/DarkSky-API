626b5f76fb93653d4b7641c9ebe3f6c6e98b80be
Martin-Louis Bright <martin.louis.bright@autodesk.com>
2017-03-01 09:49:14 -0500

    synopsis formatting

--- lib/DarkSky/API.pm
+++ lib/DarkSky/API.pm
@@ -79,33 +79,33 @@ DarkSky::API - Provides Perl API to DarkSky
 
 =head1 SYNOPSIS
 
-use 5.016;
-use DarkSky::API;
-use Data::Dumper;
-
-my $lat  = 43.6667;
-my $long = -79.4167;
-my $time = "1475363709"; # example epoch time (optional)
-my $key = "c9ce1c59d139c3dc62961cbd63097d13"; # example DarkSky API key
-
-my $forecast = DarkSky::API->new(
-    key       => $key,
-    longitude => $long,
-    latitude  => $lat,
-    time      => $time
-);
-
-say "current temperature: " . $forecast->{currently}->{temperature};
-
-my @daily_data_points = @{ $forecast->{daily}->{data} };
-
-# Use this data to prove/disprove climate change,
-# or how to understand its impact.
-# In the meantime, inspect it by dumping it.
-for (@daily_data_points) {
-    print Dumper($_);
-}
-
+    use 5.016;
+    use DarkSky::API;
+    use Data::Dumper;
+    
+    my $lat  = 43.6667;
+    my $long = -79.4167;
+    my $time = "1475363709"; # example epoch time (optional)
+    my $key = "c9ce1c59d139c3dc62961cbd63097d13"; # example DarkSky API key
+    
+    my $forecast = DarkSky::API->new(
+        key       => $key,
+        longitude => $long,
+        latitude  => $lat,
+        time      => $time
+    );
+    
+    say "current temperature: " . $forecast->{currently}->{temperature};
+    
+    my @daily_data_points = @{ $forecast->{daily}->{data} };
+    
+    # Use this data to prove/disprove climate change,
+    # or how to understand its impact.
+    # In the meantime, inspect it by dumping it.
+    for (@daily_data_points) {
+        print Dumper($_);
+    }
+    
 =head1 DESCRIPTION
 
 This module is a wrapper around the DarkSky API.
